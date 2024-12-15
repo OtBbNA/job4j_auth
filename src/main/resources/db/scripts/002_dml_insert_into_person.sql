a04f1b6336a35bf9895ef0df1cee8cda0a0eae9a
OtBbNA <tosha.kostylev.97@mail.ru>
2024-12-15 17:02:16 +0300

    1. Spring Liquibase [#302330 #525438]

--- src/main/resources/db/scripts/002_dml_insert_into_person.sql
+++ src/main/resources/db/scripts/002_dml_insert_into_person.sql
@@ -1,3 +1,3 @@
-insert into person (login, password) values ('parsentev', '123');
-insert into person (login, password) values ('ban', '123');
-insert into person (login, password) values ('ivan', '123');
+insert into person (login, password) values ('parsentev', '$2a$10$YK6YTHn9nTeI5uuI9hxuzeqm1HZFKX5dOlEHJS/KbCxIt/3/fPrvm');
+insert into person (login, password) values ('ban', '$2a$10$YK6YTHn9nTeI5uuI9hxuzeqm1HZFKX5dOlEHJS/KbCxIt/3/fPrvm');
+insert into person (login, password) values ('ivan', '$2a$10$YK6YTHn9nTeI5uuI9hxuzeqm1HZFKX5dOlEHJS/KbCxIt/3/fPrvm');
